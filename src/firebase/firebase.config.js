40162881c32d525a5b472e6cfedbc8789e3865e0
Jubayer Ahmed <sonniba7@gmail.com>
2021-10-29 18:35:27 +0600

    firebase-react-install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDBiaaWT-G6e9NCVfZb-HN_dHcYQTrekY0",
+    authDomain: "food-project-website.firebaseapp.com",
+    projectId: "food-project-website",
+    storageBucket: "food-project-website.appspot.com",
+    messagingSenderId: "1025018996560",
+    appId: "1:1025018996560:web:9bdd2459b90d01bc262e55"
+};
+
+export default firebaseConfig;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+const initializeAuthintication = () => {
+    initializeApp(firebaseConfig);
+}
+export default firebaseConfig;
